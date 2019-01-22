9b269571bf3f1e63f0bdcce22fd2ea98581ba556
Landon Gravat <railinator4903@gmail.com>
2019-01-22 23:32:12 +0000

    did some shit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Canvo
 An easy to implement, well-featured image editor that uses HTML5 Canvas.
+
+## Installation
+### Browser
+Just download `canvo.browser.js` from the `dist/` directory in this repo and include the following in the bottom of your `<body>` before any other scripts:
+```html
+<script type='text/javscript' src='/path/to/canvo.browser.js'></script>
+```
+
--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,29 @@
-import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+
+import pkg from './package.json';
 
 export default {
 	input: 'src/index.ts',
 	plugins: [
-		typescript(),
+		typescript({
+			typescript: require('typescript'),
+		}),
         terser(),
 	],
     output: [
-        { format: 'iife', name: 'Editor', file: 'dist/canvo.browser.js' },
-        { format: 'cjs', file: 'dist/canvo.cjs.js' },
-        { format: 'umd', name: 'Editor', file: 'dist/canvo.js' },
-        { format: 'es', file: 'dist/canvo.es.js' },
+        {
+			format: 'iife',
+			name: 'Editor',
+			file: 'dist/canvo.browser.js',
+		},
+        {
+			format: 'cjs',
+			file: pkg.main,
+		},
+        {
+			format: 'es',
+			file: pkg.module,
+		},
     ],
 };
